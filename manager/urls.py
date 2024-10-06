ec1fcdd848ba37e54370a416a2d8eed57f22d273
ZeroMarket <dieox2000@daum.net>
2024-10-06 17:40:10 +0900

    Update description of changes

--- manager/urls.py
+++ manager/urls.py
@@ -2,23 +2,39 @@
 from django.urls import path
 from . import views
 
+#Url 설청, 패턴정의
 urlpatterns = [
     path('', views.index, name='manager_index'),  # 기본 루트 URL을 manager 앱의 index 뷰에 연결
 ]
 
-
-
+#사용자 목록 보기 (Read)
 urlpatterns = [
     path('', views.index, name='manager_index'),
     path('users/', views.user_list, name='user_list'),  # 사용자 목록 URL 추가
 ]
 
-# manager/urls.py
-from django.urls import path
-from . import views
 
+# 사용자 추가 기능 (Create)
 urlpatterns = [
     path('', views.index, name='manager_index'),
     path('users/', views.user_list, name='user_list'),
     path('users/create/', views.user_create, name='user_create'),  # 사용자 추가 URL 정의
 ]
+
+# 용자 수정 기능 (Update)
+urlpatterns = [
+    path('', views.index, name='manager_index'),
+    path('users/', views.user_list, name='user_list'),
+    path('users/add/', views.add_user, name='add_user'),
+    path('users/edit/<int:user_id>/', views.edit_user, name='edit_user'),  # 사용자 수정 URL
+]
+
+# 사용자 삭제 기능 (Delete)
+urlpatterns = [
+    path('', views.index, name='manager_index'),
+    path('users/', views.user_list, name='user_list'),
+    path('users/add/', views.add_user, name='add_user'),
+    path('users/edit/<int:user_id>/', views.edit_user, name='edit_user'),
+    path('users/delete/<int:user_id>/', views.delete_user, name='delete_user'),  # 사용자 삭제 URL
+]
+
--- manager/views.py
+++ manager/views.py
@@ -1,11 +1,11 @@
-# manager/views.py
+# index View 함수 정의
 from django.shortcuts import render
 
 def index(request):
     return render(request, 'manager/index.html')
 
 
-# manager/views.py
+# 사용자 목록 보기 (Read)
 from django.shortcuts import render
 from .models import Users
 
@@ -14,7 +14,7 @@ def user_list(request):
     return render(request, 'manager/user_list.html', {'users': users})
 
 
-# views.py
+# 사용자 생성(Create)
 from django.shortcuts import render, redirect
 from .forms import UserCreateForm
 
@@ -27,3 +27,26 @@ def user_create(request):
     else:
         form = UserCreateForm()
     return render(request, 'manager/user_create.html', {'form': form})
+
+
+# 사용자 수정 기능(Update)
+from django.shortcuts import get_object_or_404
+
+def edit_user(request, user_id):
+    user = get_object_or_404(Users, pk=user_id)
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user_list')
+    else:
+        form = UserForm(instance=user)
+    return render(request, 'manager/edit_user.html', {'form': form})
+
+# 사용자 삭제 기능 (Delete)
+def delete_user(request, user_id):
+    user = get_object_or_404(Users, pk=user_id)
+    if request.method == 'POST':
+        user.delete()
+        return redirect('user_list')
+    return render(request, 'manager/delete_user.html', {'user': user})
